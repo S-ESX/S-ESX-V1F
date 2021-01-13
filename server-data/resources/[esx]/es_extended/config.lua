a639dae8f6c8578f7f0ed781117f6cd37b515511
Scullyy <51968381+Scullyy@users.noreply.github.com>
2021-01-13 11:58:43 +0000

    Multicharacter Support

--- server-data/resources/[esx]/es_extended/config.lua
+++ server-data/resources/[esx]/es_extended/config.lua
@@ -9,6 +9,7 @@ Config.Accounts = {
 
 Config.StartingAccountMoney = {bank = 50000}
 
+Config.MultiCharacter       = false -- enable kashacters compatibilty?
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.MaxWeight            = 24   -- the max inventory weight without backpack
